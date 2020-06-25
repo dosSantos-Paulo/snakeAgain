1764be9e3c1d167f572ba0fe68a396a5623a35c8
dosSantos-Paulo <paulo.dossantos@hotmail.com>
2020-06-25 17:16:21 -0300

    adicionado movimentação ao objeto

--- snakeScript.js
+++ snakeScript.js
@@ -20,6 +20,15 @@ function cobrinha(){
     }
 }
 
+document.addEventListener("keydown", updade)
+
+function updade (event){
+    if (event.keyCode == 37 && direction != "right") direction = "left";
+    if (event.keyCode == 38 && direction != "down") direction = "up";
+    if (event.keyCode == 39 && direction != "left") direction = "right";
+    if (event.keyCode == 40 && direction != "up") direction = "down";
+}
+
 function iniciarJogo(){
     criarBG();
     cobrinha(); 
